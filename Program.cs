f788c6919a59dd8efdd867f44dbc7c7f973604a9
jaojedat <jaojedat95@gmail.com>
2024-03-18 17:57:44 -0500

    Añadiendo cadena de conexion y creando base de datos SQL

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using EntityFramework.DBContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlServer<TasksContext>(builder.Configuration.GetConnectionString("cnTask"));
+
 var app = builder.Build();
 
+
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TasksContext DBContext) =>{
+    DBContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + DBContext.Database.IsInMemory());
+});
 
 app.Run();
