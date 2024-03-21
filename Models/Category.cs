307e510382d16bc32025ee6f16bab19fda283d8a
jaojedat <jaojedat95@gmail.com>
2024-03-21 16:33:20 -0500

    Haciendo Crud de pruebas

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EntityFramework.Models
@@ -11,6 +12,8 @@ namespace EntityFramework.Models
         public Guid CategoryId {get; set;}
         public string Name {get; set;}
         public string Descripcion {get; set;}
+
+        [JsonIgnore]
         public virtual ICollection<Task> Tasks {get; set;}
 
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using EntityFramework.DBContext;
+using EntityFramework.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,54 @@ app.MapGet("/dbconexion", async ([FromServices] TasksContext DBContext) =>{
     DBContext.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria: " + DBContext.Database.IsInMemory());
 });
+
+app.MapGet("/api/task", async ([FromServices] TasksContext DBContext) =>{
+    return Results.Ok(DBContext.Tasks.Include(c => c.Category));
+    /*var tasks = await DBContext.Tasks.Include(p => p.Category).ToListAsync();
+	return Results.Ok(tasks);*/
+});
+
+app.MapPost("/api/task", async ([FromServices] TasksContext DBContext, [FromBody] EntityFramework.Models.Task task) =>{
+    
+    task.TaskId = Guid.NewGuid();
+    task.DateCreation = DateTime.Now;
+    await DBContext.AddAsync(task);
+
+    return Results.Ok();
+});
+
+app.MapPut("/api/task/{id}", async ([FromServices] TasksContext DBContext, [FromBody] EntityFramework.Models.Task task, [FromRoute] Guid id) =>{
+    
+    var CurrentTask = DBContext.Tasks.Find(id);
+
+    if(CurrentTask != null){
+        CurrentTask.CategoryId = task.CategoryId;
+        CurrentTask.Title = task.Title;
+        CurrentTask.TaskPriority = task.TaskPriority;
+        CurrentTask.Descripcion = task.Descripcion;
+
+        await DBContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+
+    
+});
+
+app.MapDelete("/api/task/{id}", async ([FromServices] TasksContext DBContext, [FromBody] EntityFramework.Models.Task task, [FromRoute] Guid id) =>{
+    
+    var CurrentTask = DBContext.Tasks.Find(id);
+
+    if(CurrentTask != null){
+
+        DBContext.Remove(CurrentTask);
+        await DBContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
 
 app.Run();
